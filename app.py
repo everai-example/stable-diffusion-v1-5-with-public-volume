ae848b4b998786baed9b608bf33d9549b26e5435
mixatocoffeeshop <mixatocoffee@tutamail.com>
2024-08-20 20:42:54 +0800

    update readme & app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 from everai.app import App, context, VolumeRequest
 from everai_autoscaler.builtin import FreeWorkerAutoScaler
 from everai.image import Image, BasicAuth
-from everai.resource_requests import ResourceRequests
+from everai.resource_requests import ResourceRequests, CPUConstraints
 from everai.placeholder import Placeholder
 from image_builder import IMAGE
 
@@ -61,6 +61,9 @@ app = App(
         gpu_constraints=[
             "A100 40G"
         ],
+        cpu_constraints=CPUConstraints(
+            platforms=['amd64', 'arm64']
+        )
     ),
 )
 
